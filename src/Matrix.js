a9bb4b17f9331760a8351ab85aa32cdd91c91faf
congo-quinine <congo.quinine@gmail.com>
2018-12-14 09:42:11 +0000

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,32 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      color: ''
+    }
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} fetchColor={this.fetchColor}/>)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  setColor = (hexColor) => {
+    const newColor = hexColor
+    this.setState( {color: newColor} )
+  }
+
+
+  fetchColor = () => this.state.color
+
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setColor={this.setColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
@@ -34,3 +45,12 @@ export default class Matrix extends Component {
 Matrix.defaultProps = {
   values: chromeBoi
 }
+
+
+// #### `Matrix`
+// - provide a way for `Matrix` to keep track of the selected color (think state!)
+// - write a method that takes in a single argument of a hex color string (i.e.
+// '#fff') and sets the selected color to that
+// - consider what should be done with that method once it is written. Who needs it?
+// How can we get it to them? Use the skills you already have with React to solve
+// this
