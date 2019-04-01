5db1a66dcf59e99fd2819b4cd6ffdd7bf3cfcba0
Edward Sparks <edd_sparks@hotmail.com>
2019-04-01 16:07:12 +0100

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={this.props.selectColor}/>
     })
   )
-  
+
   render() {
     return (
+      <Fragment>
+      <p>Current Color</p>
+      <div key={'currentColor'} className="color-swatch" style={{backgroundColor: this.props.color}}/>
       <div id="colorSelector">
         {this.makeColorSwatches()}
       </div>
+      </Fragment>
     )
   }
-  
+
 }
